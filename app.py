34513397ef3bfe862d4cfba3c9a1b6d24987070d
Edelmir Muratkanov <muratkanov02@gmail.com>
2022-06-05 21:15:23 +0600

    Init bot

--- app.py
+++ app.py
@@ -2,15 +2,22 @@ import asyncio
 from utils.notify_admin import on_startup_notify
 from utils.set_commands import set_default_commands
 from utils.check_updates import autocheck
-from aiogram import executor
+from aiogram import Dispatcher, executor
 from handlers import dp
 from loader import db
-from data.config import DELAY
+from data.config import DELAY, WEBHOOK_PATH, WEBHOOK_URL
 
 
-async def on_startup(dp):
+async def on_startup(dp: Dispatcher):
+
+    await dp.bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
     await on_startup_notify(dp)
     await set_default_commands(dp)
+
+
+async def on_shutdown(dp: Dispatcher):
+    await dp.bot.delete_webhook()
 
 
 def repeat(coro, loop):
@@ -22,5 +29,14 @@ if __name__ == '__main__':
     db.setup()
     loop = asyncio.get_event_loop()
     loop.call_later(DELAY, repeat, autocheck, loop)
-    executor.start_polling(dp, on_startup=on_startup,
-                           loop=loop, skip_updates=True)
+    # executor.start_polling(dp, on_startup=on_startup,
+    #                        loop=loop, skip_updates=True)
+
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        loop=loop,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+    )
--- data/config.py
+++ data/config.py
@@ -7,3 +7,12 @@ APP_PATH = os.path.abspath('.')
 DELAY = 60 * 60 * 6
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = os.getenv('ADMIN_ID')
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
